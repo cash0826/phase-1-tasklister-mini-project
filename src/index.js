e7b77349d151cd66cd48eba155c097ee9a8fdeb6
Melanie Rodriguez <melarodrigg@gmail.com>
2025-10-19 18:03:48 -0600

    Attempt 1: Melanie

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+//Get Form
+const form = document.querySelector("form");
+
+//Add tasks to To-Do List
+function addTask (task) {
+  const toDoList = document.querySelector("#tasks");
+  const newTask = document.createElement("li");
+  newTask.textContent = task;
+  toDoList.appendChild(newTask);
+}
+//Attach Event Listeners
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("#new-task-description");
+    const taskText = input.value.trim();
+    if (taskText !== "") {
+      addTask(taskText);
+    };
+  form.reset();
+  });
+
+  //Delete tasks from To-Do List
+  const toDoList = document.querySelector("#tasks");
+  toDoList.addEventListener("click", (event) => {
+    if (event.target.tagName === "LI") {
+      event.target.remove();
+    }
+  });
+
 });
